4b5941b9cb874562859ca5b2330d302f52364aec
trinhdinhtai <taitd153.dev@gmail.com>
2024-12-06 00:45:42 +0700

    feat: add build:registry script, update package dependencies, and introduce Badge component for navigation

--- apps/web/components/layout/main-nav.tsx
+++ apps/web/components/layout/main-nav.tsx
@@ -1,14 +1,46 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { docsConfig } from "@/config/docs";
 import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MainNav() {
+  const pathname = usePathname();
   return (
     <div className="mr-4 hidden md:flex">
       <Link href="/" className="relative flex items-center space-x-2 lg:mr-6">
         <span className="hidden font-bold md:inline-block">
           {siteConfig.name}
         </span>
+        <Badge variant="secondary">Beta</Badge>
       </Link>
+
+      <nav className="hidden items-center space-x-6 text-sm font-medium xl:flex">
+        {docsConfig.mainNav.map((item) =>
+          item.items ? (
+            <></>
+          ) : (
+            <Link
+              key={item.href}
+              href={item.href!}
+              target={item.external ? "_blank" : undefined}
+              className={cn(
+                "hover:text-foreground/80 flex items-center justify-center transition-colors",
+                pathname === item.href
+                  ? "text-foreground"
+                  : "text-foreground/60"
+              )}
+            >
+              {item.title}
+              {item.external && <ExternalLinkIcon className="ml-2 size-4" />}
+            </Link>
+          )
+        )}
+      </nav>
     </div>
   );
 }
---
+++ apps/web/types/nav.ts
@@ -0,0 +1,24 @@
+import { Icons } from "@/components/icons";
+
+export interface NavItem {
+  title: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: keyof typeof Icons;
+  label?: string;
+  items?: NavItem[]; // Added for dropdown menus
+}
+
+export interface NavItemWithChildren extends NavItem {
+  items: NavItemWithChildren[];
+}
+
+export interface MainNavItem extends NavItem {}
+
+export interface SidebarNavItem extends NavItemWithChildren {}
+
+export interface DashboardConfig {
+  mainNav: MainNavItem[];
+  sidebarNav: SidebarNavItem[];
+}
