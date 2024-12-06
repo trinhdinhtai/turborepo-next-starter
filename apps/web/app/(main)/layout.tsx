b191a77c924f6e16f06c5ab2b00e84168f83675e
trinhdinhtai <taitd153.dev@gmail.com>
2024-12-07 00:56:38 +0700

    feat: add documentation layout and sidebar navigation components for improved documentation structure

---
+++ apps/web/app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import SiteFooter from "@/components/layout/site-footer";
+import SiteHeader from "@/components/layout/site-header";
+import { PropsWithChildren } from "react";
+
+export default async function MainLayout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <SiteHeader />
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+    </>
+  );
+}
--- apps/web/config/docs.ts
+++ apps/web/config/docs.ts
@@ -14,7 +14,7 @@ export const docsConfig: DocsConfig = {
     },
     {
       title: "Components",
-      href: "/components",
+      href: "/docs/components",
     },
     {
       title: "Themes",
