a8b47ed755d701759bcf130f788f85db2341c29b
trinhdinhtai <taitd153.dev@gmail.com>
2025-01-07 14:17:00 +0700

    feat: add core and mdx packages with TypeScript configuration and initial files; implement TOC component

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  external: [
+    "@algolia/client-search",
+    "algoliasearch",
+    "unified",
+    "next",
+    "react",
+    "react-dom",
+  ],
+  dts: true,
+  target: "es2022",
+  format: "esm",
+  entry: ["src/{toc}.tsx"],
+});
