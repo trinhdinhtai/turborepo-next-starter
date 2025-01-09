4efcf214e7dddf7f0ab451195ce07a57964dcbc8
trinhdinhtai <taitd153.dev@gmail.com>
2025-01-09 22:40:05 +0700

    feat: add i18n configuration interface and integrate with loader; update loader options to support i18n

--- packages/core/src/source/loader.ts
+++ packages/core/src/source/loader.ts
@@ -1,3 +1,4 @@
+import { I18nConfig } from "@/i18n"
 import { UrlFn } from "@/source/types"
 
 export interface LoaderConfig {
@@ -8,6 +9,10 @@ export interface LoaderOptions {
   baseUrl: string
   url?: UrlFn
   transformers?: Transformer[]
+  /**
+   * Configure i18n
+   */
+  i18n?: I18nConfig
 }
 
 export interface LoaderOutput {
@@ -23,6 +28,28 @@ export interface LoaderOutput {
   ) => (Record<TSlug, string[]> & Record<TLang, string>)[]
 }
 
+export function createGetUrl(baseUrl: string, i18n?: I18nConfig): UrlFn {
+  return (slugs, locale) => {
+    const hideLocale = i18n?.hideLocale ?? "never"
+    let urlLocale: string | undefined
+
+    if (hideLocale === "never") {
+      urlLocale = locale
+    } else if (
+      hideLocale === "default-locale" &&
+      locale !== i18n?.defaultLanguage
+    ) {
+      urlLocale = locale
+    }
+
+    const paths = urlLocale
+      ? [urlLocale, ...baseUrl.split("/"), ...slugs]
+      : [...baseUrl.split("/"), ...slugs]
+
+    return `/${paths.filter((v) => v.length > 0).join("/")}`
+  }
+}
+
 export function loader<Options extends LoaderOptions>(
   options: Options
 ): LoaderOutput {
@@ -33,6 +60,10 @@ function createOutput(options: LoaderOptions) {
   if (!options.url && !options.baseUrl) {
     console.warn("`loader()` now requires a `baseUrl` option to be defined.")
   }
+
+  const { source, rootDir = "", slugs: slugsFn = getSlugs } = options
+  const getUrl =
+    options.url ?? createGetUrl(options.baseUrl ?? "/", options.i18n)
 
   return {}
 }
