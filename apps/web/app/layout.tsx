8806486a09f516a98e6a27e7f8ac9f71a80b75ec
trinhdinhtai <taitd153.dev@gmail.com>
2025-01-15 16:51:00 +0700

    feat: refactor layout components; remove ThemeProvider and integrate next-themes in RootProvider; update layout structure for improved styling

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 
 import "./globals.css"
 
+import { ReactNode } from "react"
 import { ViewTransitions } from "next-view-transitions"
 import { RootProvider } from "tafiui/provider"
 
@@ -9,7 +10,6 @@ import { geistMono, geistSans } from "@/lib/fonts"
 import { cn, constructMetadata } from "@/lib/utils"
 import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
-import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata: Metadata = constructMetadata({
   title: "Tafi UI",
@@ -19,26 +19,24 @@ export const metadata: Metadata = constructMetadata({
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <ViewTransitions>
       <html lang="en">
-        <RootProvider>
-          <body
-            className={cn(
-              "bg-background relative flex min-h-screen w-full flex-col justify-center overflow-x-hidden scroll-smooth font-sans antialiased",
-              `${geistSans.variable} ${geistMono.variable}`
-            )}
-          >
-            <ThemeProvider attribute="class" defaultTheme="dark">
-              <TooltipProvider>
-                {children}
-                <Toaster />
-              </TooltipProvider>
-            </ThemeProvider>
-          </body>
-        </RootProvider>
+        <body
+          className={cn(
+            "flex flex-col min-h-screen bg-background font-sans",
+            `${geistSans.variable} ${geistMono.variable}`
+          )}
+        >
+          <RootProvider>
+            <TooltipProvider>
+              {children}
+              <Toaster />
+            </TooltipProvider>
+          </RootProvider>
+        </body>
       </html>
     </ViewTransitions>
   )
--- packages/ui/src/provider.tsx
+++ packages/ui/src/provider.tsx
@@ -1,13 +1,42 @@
 "use client"
 
-import type { ReactElement, ReactNode } from "react"
+import type { ComponentPropsWithoutRef, ReactElement, ReactNode } from "react"
+import { ThemeProvider } from "next-themes"
 
 export interface RootProviderProps {
+  /**
+   * Customize options of `next-themes`
+   */
+  theme?: Partial<ComponentPropsWithoutRef<typeof ThemeProvider>> & {
+    /**
+     * Enable `next-themes`
+     *
+     * @defaultValue true
+     */
+    enabled?: boolean
+  }
+
   children: ReactNode
 }
 
-export function RootProvider({ children }: RootProviderProps): ReactElement {
+export function RootProvider({
+  theme: { enabled = true, ...theme } = {},
+  children,
+}: RootProviderProps): ReactElement {
   let body = children
+
+  if (enabled)
+    body = (
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        {...theme}
+      >
+        {body}
+      </ThemeProvider>
+    )
 
   return <>{body}</>
 }
