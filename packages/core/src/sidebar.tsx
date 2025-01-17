a1a41b7da870e4ea843e64f335210b7ff9465b62
trinhdinhtai <taitd153.dev@gmail.com>
2025-01-17 17:57:32 +0700

    feat: add SidebarContext and SidebarProvider for improved sidebar management; implement SidebarTrigger and SidebarList components with scroll blocking functionality

--- packages/core/src/sidebar.tsx
+++ packages/core/src/sidebar.tsx
@@ -1,13 +1,20 @@
+"use client"
+
 import {
   createContext,
+  useContext,
+  useEffect,
   useState,
+  type ComponentPropsWithoutRef,
+  type ElementType,
   type ReactElement,
   type ReactNode,
 } from "react"
-
-type SidebarContextType = [open: boolean, setOpen: (value: boolean) => void]
+import { RemoveScroll } from "react-remove-scroll"
 
 const SidebarContext = createContext<SidebarContextType | undefined>(undefined)
+
+type SidebarContextType = [open: boolean, setOpen: (value: boolean) => void]
 
 export interface SidebarProviderProps {
   open?: boolean
@@ -15,18 +22,93 @@ export interface SidebarProviderProps {
   children: ReactNode
 }
 
-export function SidebarProvider({
-  open,
-  onOpenChange,
-  children,
-}: SidebarProviderProps): ReactElement {
+function useSidebarContext(): SidebarContextType {
+  const ctx = useContext(SidebarContext)
+  if (!ctx) throw new Error("Missing sidebar provider")
+  return ctx
+}
+
+export function SidebarProvider(
+  props: SidebarProviderProps
+): React.ReactElement {
   const [openInner, setOpenInner] = useState(false)
 
   return (
     <SidebarContext.Provider
-      value={[open ?? openInner, onOpenChange ?? setOpenInner]}
+      value={[props.open ?? openInner, props.onOpenChange ?? setOpenInner]}
     >
-      {children}
+      {props.children}
     </SidebarContext.Provider>
+  )
+}
+
+type AsProps<T extends ElementType> = Omit<
+  ComponentPropsWithoutRef<T>,
+  "as"
+> & {
+  as?: T
+}
+
+export type SidebarTriggerProps<T extends ElementType> = AsProps<T>
+
+export function SidebarTrigger<T extends ElementType = "button">({
+  as,
+  ...props
+}: SidebarTriggerProps<T>): ReactElement {
+  const [open, setOpen] = useSidebarContext()
+  const As = as ?? "button"
+
+  return (
+    <As
+      aria-label="Toggle Sidebar"
+      data-open={open}
+      onClick={() => {
+        setOpen(!open)
+      }}
+      {...props}
+    />
+  )
+}
+
+export type SidebarContentProps<T extends ElementType> = AsProps<T> & {
+  /**
+   * Disable scroll blocking when the viewport width is larger than a certain number (in pixels)
+   */
+  blockScrollingWidth?: number
+}
+
+export function SidebarList<T extends ElementType = "aside">({
+  as,
+  blockScrollingWidth,
+  ...props
+}: SidebarContentProps<T>): ReactElement {
+  const [open] = useSidebarContext()
+  const [isBlocking, setIsBlocking] = useState(false)
+
+  useEffect(() => {
+    if (!blockScrollingWidth) return
+    const mediaQueryList = window.matchMedia(
+      `(min-width: ${blockScrollingWidth.toString()}px)`
+    )
+
+    const handleChange = (): void => {
+      setIsBlocking(!mediaQueryList.matches)
+    }
+    handleChange()
+    mediaQueryList.addEventListener("change", handleChange)
+    return () => {
+      mediaQueryList.removeEventListener("change", handleChange)
+    }
+  }, [blockScrollingWidth])
+
+  return (
+    <RemoveScroll
+      as={as ?? "aside"}
+      data-open={open}
+      enabled={Boolean(isBlocking && open)}
+      {...props}
+    >
+      {props.children}
+    </RemoveScroll>
   )
 }
